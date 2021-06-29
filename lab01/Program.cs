7cd4bbfbaa221d1b1a0d8fd8170c41ad1e15576a
Irin Krapookthong (NTL) <CF303520@tidlor.com>
2021-06-29 11:01:08 +0700

    init

---
+++ lab01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******REGISTER*******");
+
+            Console.Write("Please Enter your ID : ");
+            var number = Console.ReadLine();
+
+            Console.Write("Please Enter your Name : ");
+            var name = Console.ReadLine();
+
+            Console.Write("Register Complete:");
+            Console.WriteLine(number + " " + name);
+
+            Console.ReadLine();
+        }
+    }
+}

9ddeeddb8180f3c0dcc903189fa076c33a478cef
gaitong <gaitong.p@gmail.com>
2021-07-01 14:19:04 +0700

    add class register

--- lab01/Program.cs
+++ lab01/Program.cs
@@ -12,16 +12,24 @@ namespace lab01
         {
             Console.WriteLine("******REGISTER*******");
 
-            Console.Write("Please Enter your ID : ");
-            var number = Console.ReadLine();
+            //Console.Write("Please Enter your ID : ");
+            //var number = Console.ReadLine();
 
-            Console.Write("Please Enter your Name : ");
-            var name = Console.ReadLine();
+            //Console.Write("Please Enter your Name : ");
+            //var name = Console.ReadLine();
 
-            Console.Write("Register Complete:");
-            Console.WriteLine(number + " " + name);
+            //Console.Write("Register Complete:");
+            //Console.WriteLine(number + " " + name);
 
-            Console.ReadLine();
+            //Console.ReadLine();
+
+            var reg1 = new Register("11316684","Gaitong Phansanit");
+            var id = reg1.PrintId();
+            var name = reg1.PrintName();
+
+            Console.WriteLine("Your Id : " + id);
+            Console.WriteLine("Your Name : " + name);
+            reg1.ShowMessage();
         }
     }
 }
---
+++ lab01/Register.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    public class Register
+    {
+        //property
+        private string Id = "";
+        private string Name = "";
+
+        //contrucor
+        public Register(string id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        //method return string
+        public string PrintId()
+        {
+            return this.Id;
+        }
+
+        public string PrintName()
+        {
+            return this.Name;
+        }
+
+        //method
+        public void ShowMessage()
+        {
+            Console.WriteLine("register Success : "+Id + " "+Name);
+        }
+    }
+}
